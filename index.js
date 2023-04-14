056293a392b16d8ef900e96dc859851e9db79d63
Nicolas <nicobarbosa20034@gmail.com>
2023-04-14 20:28:03 -0300

    adicionando cors

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const cors = require("cors") // npm i cors
+app.use(cors())
+
 app.get('/', (req, res) => {
     res.send('Olá Mundo')
 })
